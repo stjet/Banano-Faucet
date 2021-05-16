596ae77215b73994f03f846d56f5fb4fad7634f9
Jon Dough <>
2021-05-16 01:55:45 +0000

    Initial commit

---
+++ banano.js
@@ -0,0 +1,31 @@
+const bananojs = require('@bananocoin/bananojs');
+bananojs.setBananodeApiUrl('https://kaliumapi.appditto.com/api');
+
+async function send_banano(addr, amount) {
+  try {
+    await bananojs.sendBananoWithdrawalFromSeed(process.env.seed, 0, addr, amount);
+    return true;
+  } catch (e) {
+    return false;
+  }
+}
+
+async function check_bal(addr) {
+  let raw_bal = await bananojs.getAccountBalanceRaw(addr);
+  let bal_parts = await bananojs.getBananoPartsFromRaw(raw_bal);
+  return bal_parts.banano
+}
+
+async function faucet_dry() {
+  let bal = await check_bal("ban_3346kkobb11qqpo17imgiybmwrgibr7yi34mwn5j6uywyke8f7fnfp94uyps");
+  console.log(bal)
+  if (Number(bal) < 1) {
+    return true;
+  }
+  return false;
+}
+
+module.exports = {
+  send_banano: send_banano,
+  faucet_dry: faucet_dry
+}
---
+++ index.js
@@ -0,0 +1,115 @@
+const swig = require('swig');
+const express = require('express');
+const axios = require('axios')
+
+const Database = require("@replit/database")
+
+const db = new Database()
+
+const cookieParser = require('cookie-parser');
+const bodyParser = require('body-parser')
+
+const banano = require('./banano.js')
+
+const app = express();
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
+
+app.use(cookieParser());
+
+app.get('/', function (req, res) {
+  let errors = false;
+  let address = false;
+  let given = false;
+  let amount = 0;
+  //render template 
+  return res.send(swig.renderFile("./templates/index.html", {errors: errors, address: address, given: given, amount: amount}));
+})
+
+app.post('/', async function (req, res) {
+  let errors = false;
+  let address = false;
+  let given = false;
+  let amount = (Math.floor(Math.random()*9)/100)+0.01;
+  let token = req.body['h-captcha-response'];
+  address = req.body['addr'];
+  let params = new URLSearchParams();
+  params.append('response', token);
+  params.append('secret', process.env.secret);
+  let captcha_resp = await axios.post('https://hcaptcha.com/siteverify', params)
+  captcha_resp = captcha_resp.data;
+  let dry = await banano.faucet_dry()
+  if (captcha_resp['success'] && !dry) {
+    //check cookie
+    if (req.cookies['last_claim']){
+      if (Number(req.cookies['last_claim'])+86400000 < Date.now()) {
+        let db_result = await db.get(address);
+        if (db_result) {
+          if (Number(db_result)+86400000 < Date.now()) {
+            //all clear, send bananos!
+            send = await banano.send_banano(address, amount);
+            if (send == false) {
+              errors = "Invalid address"
+            } else {
+              res.cookie('last_claim', String(Date.now()));
+              await db.set(address,String(Date.now()));
+              given = true;
+            }
+          } else {
+            errors = "Last claim too soon"
+          }
+        } else {
+          await db.set(address,String(Date.now()));
+          //all clear, send bananos!
+          send = await banano.send_banano(address, amount);
+          if (send == false) {
+            errors = "Invalid address"
+          } else {
+            res.cookie('last_claim', String(Date.now()));
+            await db.set(address,String(Date.now()));
+            given = true;
+          }
+        }
+      } else {
+        //add errors
+        errors = "Last claim too soon"
+      }
+    } else {
+      //check db 
+      let db_result = await db.get(address);
+      if (db_result) {
+        if (Number(db_result)+86400000 < Date.now()) {
+          //all clear, send bananos!
+          send = await banano.send_banano(address, amount);
+          if (send == false) {
+            errors = "Invalid address"
+          } else {
+            res.cookie('last_claim', String(Date.now()));
+            await db.set(address,String(Date.now()));
+            given = true;
+          }
+        } else {
+          errors = "Last claim too soon"
+        }
+      } else {
+        //all clear, send bananos!
+        send = await banano.send_banano(address, amount);
+        if (send == false) {
+          errors = "Invalid address"
+        } else {
+          res.cookie('last_claim', String(Date.now()));
+          await db.set(address,String(Date.now()));
+          given = true;
+        }
+      }
+    }
+  } else {
+    errors = "captcha incorrect or faucet dry"
+  }
+  return res.send(swig.renderFile("./templates/index.html", {errors: errors, address: address, given: given, amount: amount}));
+})
+
+app.listen(8081, () => {
+  console.log(`App on`)
+})
